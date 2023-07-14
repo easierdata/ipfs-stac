ab35c5e3d666e9c6e54d31f53f96ee776c9d2b1d
matthewnanas <spazeplayz@gmail.com>
2023-07-14 10:15:18 -0400

    Update Pypi description

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import find_packages, setup
 
 setup(
     name='easier_python_client',
-    packages=find_packages(),
-    version='0.1.0',
+    packages=find_packages(include='easier_python_client'),
+    version='0.1.1',
     description='The EASIER Data Initiative Python IPFS Client',
     author='The EASIER Data Initiative',
     license='MIT',
-    install_requires=[],
+    install_requires=['fsspec', 'requests', 'pandas', 'beautifulsoup4', 'pystac-client'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-    test_suite='tests'
+    test_suite='tests',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
 )
