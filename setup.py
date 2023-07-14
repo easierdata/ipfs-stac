a0b1f524bea6545438745ba80317e3754425976d
matthewnanas <spazeplayz@gmail.com>
2023-07-14 10:20:19 -0400

    Bug fix

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='easier_python_client',
-    packages=find_packages(include='easier_python_client'),
-    version='0.1.1',
+    packages=find_packages(include=['easier_python_client']),
+    version='0.1.2',
     description='The EASIER Data Initiative Python IPFS Client',
     author='The EASIER Data Initiative',
     license='MIT',
