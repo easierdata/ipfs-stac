0a7604cb46f8655fabf13400ae4a7b137919ffdb
matthewnanas <spazeplayz@gmail.com>
2023-07-20 19:13:19 -0400

    Add project urls

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='ipfs_stac',
     packages=find_packages(include=['ipfs_stac']),
-    version='0.1.5',
+    version='0.1.6',
     description='The EASIER Data Initiative Python IPFS-STAC Client',
     author='The EASIER Data Initiative',
     license='MIT',
@@ -12,5 +12,9 @@ setup(
     tests_require=['pytest'],
     test_suite='tests',
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    project_urls={
+        "Twitter": "https://twitter.com/easierdataorg",
+        "GitHub": "https://github.com/easierdata/ipfs-stac"
+    }
 )
