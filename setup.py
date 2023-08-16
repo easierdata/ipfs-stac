9058caaf0eec8c6b755c482b81db95ce52b35808
matthewnanas <spazeplayz@gmail.com>
2023-08-16 12:25:13 -0400

    Versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='ipfs_stac',
     packages=find_packages(include=['ipfs_stac']),
-    version='0.1.7',
+    version='0.1.8',
     description='The EASIER Data Initiative Python IPFS-STAC Client',
     author='The EASIER Data Initiative',
     license='MIT',
