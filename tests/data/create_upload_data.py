1a1f438522af2aea291fb2e92c46cafad9e855a3
SethDocherty <sddocherty@gmail.com>
2024-10-23 19:45:07 -0400

    get filepath in system agnostic way + moved the import statment into create image file method

--- tests/data/create_upload_data.py
+++ tests/data/create_upload_data.py
@@ -1,6 +1,7 @@
-from PIL import Image
 from io import BytesIO
 import subprocess
+from pathlib import Path
+
 
 def create_text_file():
     with open("hello.txt", "w") as f:
@@ -10,10 +11,13 @@ def create_text_file():
 
 def create_image_file():
     # Create a 50x50 image
-    with open("tests/data/image.png", "wb") as f:
+    img_path = Path("tests", "data", "image.png").as_posix()
+    from PIL import Image
+
+    with open(img_path, "wb") as f:
         image = Image.new("RGB", (50, 50))
         image_bytes = BytesIO()
-        image.save(image_bytes, format='PNG')
+        image.save(image_bytes, format="PNG")
         image_bytes.seek(0)
         f.write(image_bytes.read())
-        subprocess.run(["ipfs", "add", "tests/data/image.png"])
+        subprocess.run(["ipfs", "add", img_path])
